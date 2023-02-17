74e54f745ab07efe9299f448103b46fa9f31b0e1
Okpainmo <okpainmoandrew@gmail.com>
2023-02-17 14:36:04 +0100

    refactor: updates on the post page and the blog hompage

--- client/components/blog/BlogHome.jsx
+++ client/components/blog/BlogHome.jsx
@@ -4,25 +4,157 @@ import Emoji from '../Emoji';
 
 function BlogHome() {
   return (
-    <div className="pt-20 sm:pt-24 md:pt-32 px-3 xsm:w-[85%] xsm:mx-auto lg:w-[70%]">
-      <section className=" blog-intro border-b-2 pb-10 md:pb-20">
-        <h1 className="poppins font-bold text-3xl md:text-4xl">
-          Hey <Emoji symbol="👋" label="waving-hand" />, <br />
-          Welcome to my blog.{' '}
-        </h1>
-        <p className="text--colors_secondary mt-4">
-          <span className="text-lg font-bold montserrat">
-            A minimalist blog about everything "software engineering" - From developer health, to
-            software engineering resources, content creation, and more.
-          </span>
-          {/* <br /> <br />
+    <div className="">
+      <section className="blog_bg--primary px-3 pt-20 sm:pt-24 md:pt-32 xsm:px-[20px]">
+        <div className="blog-intro pb-10 md:pb-20 sm:w-[80%] xsm:mx-auto">
+          <h1 className="poppins font-bold text-3xl md:text-4xl">
+            Hey <Emoji symbol="👋" label="waving-hand" />, <br />
+            Welcome to my blog.{' '}
+          </h1>
+          <p className="mt-4">
+            <span className="text-lg font-bold montserrat">
+              A minimalist blog about everything "software engineering" - From developer health, to
+              software engineering resources, content creation, and more.
+            </span>
+            {/* <br /> <br />
           <span className="underline">Enjoy your time here.</span> */}
-        </p>
+          </p>
+        </div>
       </section>
-      <section className="flex flex-col lg:flex-row md:w-[80%] lg:w-full lg:justify-between">
-        <section className="recent-post-wrapper mt-6 mb-12 md:mt-16 text-[12px] sm:text-[14px] lg:w-[60%]">
+      <section className="xsm:px-[20px] sm:w-[80%] sm:mx-auto px-3">
+        <div className="recent-posts flex flex-col lg:flex-row md:w-[80%] lg:w-full lg:justify-between">
+          <section className="recent-post-wrapper mt-6 mb-12 md:mt-16 text-[12px] sm:text-[14px] lg:w-[60%]">
+            <h3 className="uppercase font-bold text--colors_secondary mb-4 text-lg montserrat">
+              Recent Posts
+            </h3>
+            <div>
+              <div className="post mt-4 md:mb-16">
+                <div className="post-title text-[17px] font-bold poppins">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing.
+                </div>
+                <div className="post-brief mt-2">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quod sequi
+                  nostrum nulla expedita. Iste quidem dolor at fuga sunt.
+                </div>
+                <div className="post-link mt-2 font-bold poppins">
+                  <Link className="underline text--colors_primary" href="/blog/post">
+                    Read post
+                  </Link>
+                </div>
+              </div>
+              <div className="post mt-12 md:mb-16">
+                <div className="post-title text-[17px] font-bold poppins">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing.
+                </div>
+                <div className="post-brief mt-2">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quod sequi
+                  nostrum nulla expedita. Iste quidem dolor at fuga sunt.
+                </div>
+                <div className="post-link mt-2 font-bold poppins">
+                  <a className="underline text--colors_primary" href="g.com">
+                    Read post
+                  </a>
+                </div>
+              </div>
+              <div className="post mt-12 md:mb-16">
+                <div className="post-title text-[17px] font-bold poppins">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing.
+                </div>
+                <div className="post-brief mt-2">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quod sequi
+                  nostrum nulla expedita. Iste quidem dolor at fuga sunt.
+                </div>
+                <div className="post-link mt-2 font-bold poppins">
+                  <a className="underline text--colors_primary" href="g.com">
+                    Read post
+                  </a>
+                </div>
+              </div>
+              <div className="post mt-12 md:mb-16">
+                <div className="post-title text-[17px] font-bold poppins">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing.
+                </div>
+                <div className="post-brief mt-2">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quod sequi
+                  nostrum nulla expedita. Iste quidem dolor at fuga sunt.
+                </div>
+                <div className="post-link mt-2 font-bold poppins">
+                  <a className="underline text--colors_primary" href="g.com">
+                    Read post
+                  </a>
+                </div>
+              </div>
+              <div className="post mt-12 md:mb-16">
+                <div className="post-title text-[17px] font-bold poppins">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing.
+                </div>
+                <div className="post-brief mt-2">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quod sequi
+                  nostrum nulla expedita. Iste quidem dolor at fuga sunt.
+                </div>
+                <div className="post-link mt-2 font-bold poppins">
+                  <a className="underline text--colors_primary" href="g.com">
+                    Read post
+                  </a>
+                </div>
+              </div>
+              <div className="post mt-12">
+                <div className="post-title text-[17px] font-bold poppins">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing.
+                </div>
+                <div className="post-brief mt-2">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quod sequi
+                  nostrum nulla expedita. Iste quidem dolor at fuga sunt.
+                </div>
+                <div className="post-link mt-2 font-bold poppins">
+                  <a className="underline text--colors_primary" href="g.com">
+                    Read post
+                  </a>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="categories-wrapper mt-8 text-[12px] sm:text-[14px] border-t-2 py-8 lg:border-t-0 lg:w-[30%] font-bold montserrat">
+            <h3 className="uppercase font-bold text--colors_secondary mb-2 text-lg">
+              All categories
+            </h3>
+            <div className="categories w-full flex flex-wrap gap-x-3 md:w-[90%]">
+              <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
+                firebase
+              </div>
+              <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
+                firebase
+              </div>
+              <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
+                firebase
+              </div>
+              <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
+                firebase
+              </div>
+              <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
+                firebase
+              </div>
+              <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
+                firebase
+              </div>
+              <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
+                firebase
+              </div>
+              <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
+                firebase
+              </div>
+              <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
+                firebase
+              </div>
+              <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
+                firebase
+              </div>
+            </div>
+          </section>
+        </div>
+        <section className="most-loved-wrapper mt-6 mb-16 text-[12px] sm:text-[14px] border-t-2 pt-8 md:w-[80%] lg:w-[60%]">
           <h3 className="uppercase font-bold text--colors_secondary mb-4 text-lg montserrat">
-            Recent Posts
+            Most loved posts
           </h3>
           <div>
             <div className="post mt-4 md:mb-16">
@@ -111,134 +243,6 @@ function BlogHome() {
             </div>
           </div>
         </section>
-        <section className="categories-wrapper mt-8 text-[12px] sm:text-[14px] border-t-2 py-8 lg:border-t-0 lg:w-[35%] font-bold montserrat">
-          <h3 className="uppercase font-bold text--colors_secondary mb-2 text-lg">
-            All categories
-          </h3>
-          <div className="categories w-full flex flex-wrap gap-x-3 md:w-[90%]">
-            <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
-              firebase
-            </div>
-            <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
-              firebase
-            </div>
-            <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
-              firebase
-            </div>
-            <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
-              firebase
-            </div>
-            <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
-              firebase
-            </div>
-            <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
-              firebase
-            </div>
-            <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
-              firebase
-            </div>
-            <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
-              firebase
-            </div>
-            <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
-              firebase
-            </div>
-            <div className="category-tag bg--primary px-3 py-2 text--colors_white mt-4 text-[10px] rounded">
-              firebase
-            </div>
-          </div>
-        </section>
-      </section>
-      <section className="most-loved-wrapper mt-6 mb-16 text-[12px] sm:text-[14px] border-t-2 pt-8 md:w-[80%] lg:w-[60%]">
-        <h3 className="uppercase font-bold text--colors_secondary mb-4 text-lg montserrat">
-          Most loved posts
-        </h3>
-        <div>
-          <div className="post mt-4 md:mb-16">
-            <div className="post-title text-[17px] font-bold poppins">
-              Lorem ipsum dolor, sit amet consectetur adipisicing.
-            </div>
-            <div className="post-brief mt-2">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quod sequi
-              nostrum nulla expedita. Iste quidem dolor at fuga sunt.
-            </div>
-            <div className="post-link mt-2 font-bold poppins">
-              <Link className="underline text--colors_primary" href="/blog/post">
-                Read post
-              </Link>
-            </div>
-          </div>
-          <div className="post mt-12 md:mb-16">
-            <div className="post-title text-[17px] font-bold poppins">
-              Lorem ipsum dolor, sit amet consectetur adipisicing.
-            </div>
-            <div className="post-brief mt-2">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quod sequi
-              nostrum nulla expedita. Iste quidem dolor at fuga sunt.
-            </div>
-            <div className="post-link mt-2 font-bold poppins">
-              <a className="underline text--colors_primary" href="g.com">
-                Read post
-              </a>
-            </div>
-          </div>
-          <div className="post mt-12 md:mb-16">
-            <div className="post-title text-[17px] font-bold poppins">
-              Lorem ipsum dolor, sit amet consectetur adipisicing.
-            </div>
-            <div className="post-brief mt-2">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quod sequi
-              nostrum nulla expedita. Iste quidem dolor at fuga sunt.
-            </div>
-            <div className="post-link mt-2 font-bold poppins">
-              <a className="underline text--colors_primary" href="g.com">
-                Read post
-              </a>
-            </div>
-          </div>
-          <div className="post mt-12 md:mb-16">
-            <div className="post-title text-[17px] font-bold poppins">
-              Lorem ipsum dolor, sit amet consectetur adipisicing.
-            </div>
-            <div className="post-brief mt-2">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quod sequi
-              nostrum nulla expedita. Iste quidem dolor at fuga sunt.
-            </div>
-            <div className="post-link mt-2 font-bold poppins">
-              <a className="underline text--colors_primary" href="g.com">
-                Read post
-              </a>
-            </div>
-          </div>
-          <div className="post mt-12 md:mb-16">
-            <div className="post-title text-[17px] font-bold poppins">
-              Lorem ipsum dolor, sit amet consectetur adipisicing.
-            </div>
-            <div className="post-brief mt-2">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quod sequi
-              nostrum nulla expedita. Iste quidem dolor at fuga sunt.
-            </div>
-            <div className="post-link mt-2 font-bold poppins">
-              <a className="underline text--colors_primary" href="g.com">
-                Read post
-              </a>
-            </div>
-          </div>
-          <div className="post mt-12">
-            <div className="post-title text-[17px] font-bold poppins">
-              Lorem ipsum dolor, sit amet consectetur adipisicing.
-            </div>
-            <div className="post-brief mt-2">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quod sequi
-              nostrum nulla expedita. Iste quidem dolor at fuga sunt.
-            </div>
-            <div className="post-link mt-2 font-bold poppins">
-              <a className="underline text--colors_primary" href="g.com">
-                Read post
-              </a>
-            </div>
-          </div>
-        </div>
       </section>
     </div>
   );
--- client/components/blog/PostPage.jsx
+++ client/components/blog/PostPage.jsx
@@ -3,58 +3,114 @@ import React from 'react';
 function PostPage() {
   return (
     <div className="">
-      <section className="post-top pb-10 post-page_bg--light px-3 pt-40">
-        <div className="tags">
-          <div className="tags w-full flex flex-wrap gap-x-3 text--colors_white">
-            <div className="rounded border bg--secondary px-3 py-1.5 text--colors_white mt-4 text-[10px]">
-              firebase
-            </div>
-            <div className="rounded border bg--secondary px-3 py-1.5 text--colors_white mt-4 text-[10px]">
-              firebase
-            </div>
-            <div className="rounded border bg--secondary px-3 py-1.5 text--colors_white mt-4 text-[10px]">
-              firebase
-            </div>
-            <div className="rounded border bg--secondary px-3 py-1.5 text--colors_white mt-4 text-[10px]">
-              firebase
-            </div>
-            <div className="rounded border bg--secondary px-3 py-1.5 text--colors_white mt-4 text-[10px]">
-              firebase
-            </div>
-            <div className="rounded border bg--secondary px-3 py-1.5 text--colors_white mt-4 text-[10px]">
-              firebase
+      <section className="post-top pb-10 px-3 pt-40 blog_bg--primary xsm:px-[20px]">
+        <div className="sm:w-[80%] sm:mx-auto">
+          <div className="tags">
+            <div className="tags w-full flex flex-wrap gap-x-3 text--colors_white">
+              <div className="rounded border bg--secondary px-3 py-1.5 text--colors_white mt-4 text-[10px]">
+                firebase
+              </div>
+              <div className="rounded border bg--secondary px-3 py-1.5 text--colors_white mt-4 text-[10px]">
+                firebase
+              </div>
+              <div className="rounded border bg--secondary px-3 py-1.5 text--colors_white mt-4 text-[10px]">
+                firebase
+              </div>
+              <div className="rounded border bg--secondary px-3 py-1.5 text--colors_white mt-4 text-[10px]">
+                firebase
+              </div>
+              <div className="rounded border bg--secondary px-3 py-1.5 text--colors_white mt-4 text-[10px]">
+                firebase
+              </div>
+              <div className="rounded border bg--secondary px-3 py-1.5 text--colors_white mt-4 text-[10px]">
+                firebase
+              </div>
             </div>
           </div>
-        </div>
-        <h1 className="post-title text-2xl poppins font-bold pb-3 pt-3">
-          How I built my minimalitic blog-folio web-app.
-        </h1>
-        <div className="post-details text-[12px]">
-          <div className="post-author poppins text--colors_primary font-bold">Andrew Okpainmo</div>
-          <div className="post-author font-bold">23rd June 2023</div>
+          <h1 className="post-title text-2xl md:text-3xl poppins font-bold pb-3 pt-3">
+            How I built my minimalist blog-folio web-app.
+          </h1>
+          <div className="post-details text-[12px]">
+            <div className="post-author poppins text--colors_primary font-bold">
+              Andrew Okpainmo
+            </div>
+            <div className="post-author font-bold">23rd June 2023</div>
+          </div>
         </div>
       </section>
-      <section className="post-body px-3 mt-8 pb-16">
-        <h3 className="post-section-title font-bold poppins text-lg">Introduction</h3>
-        <p className="post-serif text-[14px] mt-2 leading-[27px]">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate animi tempore, aperiam
-          praesentium necessitatibus fuga dolorum molestias ipsum esse ipsa harum, voluptatem
-          tenetur. Numquam incidunt doloremque itaque, ad ex unde suscipit magni accusamus autem quo
-          consequuntur, error labore, dolorum dolore dicta sunt. Amet tempora esse, rerum sint
-          similique magnam expedita voluptas quod aperiam pariatur. Consectetur fuga perferendis
-          odit facere eligendi hic ratione mollitia ut illo consequatur qui nihil ex voluptatibus
-          voluptas enim consequuntur blanditiis asperiores, culpa nemo, suscipit debitis laboriosam
-          veritatis quos. Perferendis laudantium magni dicta. Aspernatur, debitis sunt sit vitae
-          commodi est possimus culpa blanditiis! Animi illo atque nisi aspernatur distinctio impedit
-          consectetur eos temporibus ducimus nobis architecto perspiciatis beatae, quasi non
-          quaerat, assumenda sed optio numquam. Blanditiis ab harum et! Veniam explicabo molestiae
-          delectus ipsum aliquid! Ex fuga inventore odit blanditiis iste repellat maxime eos sed
-          dolore placeat autem, nulla nemo possimus similique, ab obcaecati quae, unde sapiente quas
-          nihil distinctio ad beatae quo. Voluptas commodi placeat culpa. Sint a sed, ipsum
-          sapiente, esse consequatur tempore odit pariatur dolore adipisci nisi blanditiis laborum
-          modi perferendis fugiat quo est excepturi, aliquid alias neque maiores quod voluptas id.
-          Unde id asperiores vitae ex illum magni mollitia est deserunt laborum eius.
-        </p>
+      <section className="post-body text-[14px] px-3 xsm:px-[20px] mt-8 pb-16 sm:w-[80%] sm:mx-auto lg:flex lg:justify-between">
+        <div className="lg:w-[65%]">
+          <h3 className="post-section-title font-bold poppins text-lg">Introduction</h3>
+          <p className="post-serif mt-2 leading-[27px] md:leading-[30px]">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate animi tempore,
+            aperiam praesentium necessitatibus fuga dolorum molestias ipsum esse ipsa harum,
+            voluptatem tenetur. Numquam incidunt doloremque itaque, ad ex unde suscipit magni
+            accusamus autem quo consequuntur, error labore, dolorum dolore dicta sunt. Amet tempora
+            esse, rerum sint similique magnam expedita voluptas quod aperiam pariatur. Consectetur
+            fuga perferendis odit facere eligendi hic ratione mollitia ut illo consequatur qui nihil
+            ex voluptatibus voluptas enim consequuntur blanditiis asperiores, culpa nemo, suscipit
+            debitis laboriosam veritatis quos. Perferendis laudantium magni dicta. Aspernatur,
+            debitis sunt sit vitae commodi est possimus culpa blanditiis! Animi illo atque nisi
+            aspernatur distinctio impedit consectetur eos temporibus ducimus nobis architecto
+            perspiciatis beatae, quasi non quaerat, assumenda sed optio numquam. Blanditiis ab harum
+            et! Veniam explicabo molestiae delectus ipsum aliquid! Ex fuga inventore odit blanditiis
+            iste repellat maxime eos sed dolore placeat autem, nulla nemo possimus similique, ab
+            obcaecati quae, unde sapiente quas nihil distinctio ad beatae quo. Voluptas commodi
+            placeat culpa. Sint a sed, ipsum sapiente, esse consequatur tempore odit pariatur dolore
+            adipisci nisi blanditiis laborum modi perferendis fugiat quo est excepturi, aliquid
+            alias neque maiores quod voluptas id. Unde id asperiores vitae ex illum magni mollitia
+            est deserunt laborum eius.
+          </p>
+        </div>
+        <div className="table-of-contents hidden lg:inline-block lg:w-[20%]">
+          <h3 className="montserrat font-bold text--colors_secondary text-lg mb-3">
+            Table of contents
+          </h3>
+          <ul className="flex-col gap-y-3 lg:flex list-none post-serif lg:justify-between text-[12px]">
+            <li
+              className="relative before:w-[7px]
+           before:h-[7px] before:block before:bg--secondary before:absolute before:left-[-20px] before:top-[5px]"
+            >
+              Introduction.
+            </li>
+            <li
+              className="relative before:w-[7px]
+           before:h-[7px] before:block before:bg--secondary before:absolute before:left-[-20px] before:top-[5px]"
+            >
+              Lorem, ipsum dolor.
+            </li>
+            <li
+              className="relative before:w-[7px]
+           before:h-[7px] before:block before:bg--secondary before:absolute before:left-[-20px] before:top-[5px]"
+            >
+              Lorem, ipsum dolor.
+            </li>
+            <li
+              className="relative before:w-[7px]
+           before:h-[7px] before:block before:bg--secondary before:absolute before:left-[-20px] before:top-[5px]"
+            >
+              Lorem, ipsum dolor.
+            </li>
+            <li
+              className="relative before:w-[7px]
+           before:h-[7px] before:block before:bg--secondary before:absolute before:left-[-20px] before:top-[5px]"
+            >
+              Lorem, ipsum dolor.
+            </li>
+            <li
+              className="relative before:w-[7px]
+           before:h-[7px] before:block before:bg--secondary before:absolute before:left-[-20px] before:top-[5px]"
+            >
+              Lorem, ipsum dolor.
+            </li>
+            <li
+              className="relative before:w-[7px]
+           before:h-[7px] before:block before:bg--secondary before:absolute before:left-[-20px] before:top-[5px]"
+            >
+              Conclusion.
+            </li>
+          </ul>
+        </div>
       </section>
     </div>
   );
