3c10c9a6e9903a0488e59d900bc00665d2c59a64
Okpainmo <okpainmoandrew@gmail.com>
2023-02-17 07:57:46 +0100

    refactor(pages): added a serif font to enhance readbility of blog-post section + minor CSS update

--- client/pages/_document.jsx
+++ client/pages/_document.jsx
@@ -5,9 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Nunito+Sans:wght@300;400&family=Poppins:wght@400;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Montserrat:wght@400;700&family=Nunito+Sans:wght@300;400&family=Poppins:wght@400;800&display=swap"
           rel="stylesheet"
         />
       </Head>
